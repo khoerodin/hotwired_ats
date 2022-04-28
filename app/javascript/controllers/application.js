0c86987f7228e216f5fdca2289491c09733cd6f0
Khoerodin <khoerodin@live.com>
2022-04-28 09:20:16 +0700

    Install CableReady, StimulusReflex, and Mrujs

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,8 +1,10 @@
 import { Application } from "@hotwired/stimulus"
+import StimulusReflex from "stimulus_reflex"
 
 const application = Application.start()
 
 // Configure Stimulus development experience
+application.warnings = true
 application.debug = false
 window.Stimulus   = application
 
