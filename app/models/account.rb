be3fb76c86469cf806354b344f63f6c8fbc7cb92
Khoerodin <khoerodin@live.com>
2022-04-29 13:08:44 +0700

    Scaffold jobs

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,5 +1,6 @@
 class Account < ApplicationRecord
   validates :name, presence: true
 
+  has_many :jobs, dependent: :destroy
   has_many :users, dependent: :destroy
 end
---
+++ app/models/job.rb
@@ -0,0 +1,18 @@
+class Job < ApplicationRecord
+  belongs_to :account
+
+  validates :title, :location, :status, :job_type, presence: true
+
+  enum status: {
+    draft: "draft",
+    open: "open",
+    closed: "closed"
+  }
+
+  enum job_type: {
+    full_time: "full_time",
+    part_time: "part_time"
+  }
+
+  has_rich_text :description
+end
