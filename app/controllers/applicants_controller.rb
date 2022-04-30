25fd17853a8f32ecfa963b7aef8773640e085b6f
Khoerodin <khoerodin@live.com>
2022-04-30 15:43:46 +0700

    Dragging applicants with Stimulus

--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -1,6 +1,6 @@
 class ApplicantsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_applicant, only: %i[show edit update destroy]
+  before_action :set_applicant, only: %i[show edit update destroy change_stage]
 
   # GET /applicants
   def index
@@ -57,6 +57,11 @@ class ApplicantsController < ApplicationController
       format.html { redirect_to applicants_url, notice: "Applicant was successfully destroyed." }
       format.json { head :no_content }
     end
+  end
+
+  def change_stage
+    @applicant.update(applicant_params)
+    head :ok
   end
 
   private
