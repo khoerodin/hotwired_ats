fe0a705c12b31cb40d41f60c38d920b3823778fb
Khoerodin <khoerodin@live.com>
2022-05-07 17:05:24 +0700

    Cleaning up the filter form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,12 @@ module ApplicationHelper
       "bg-gray-900 border-blue-900"
     end
   end
+
+  def job_options_for_select(account_id)
+    Job.where(account_id: account_id).order(:title).pluck(:title, :id)
+  end
+
+  def fetch_filter_key(resource, user_id, key)
+    Kredis.hash("#{resource}_filters:#{user_id}")[key]
+  end
 end
