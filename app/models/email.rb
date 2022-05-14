b0deef6cad6953428c25c65fcf6a40d1e5c0678b
Khoerodin <khoerodin@live.com>
2022-05-14 08:22:54 +0700

    Receive and process inbound email

--- app/models/email.rb
+++ app/models/email.rb
@@ -2,11 +2,16 @@ class Email < ApplicationRecord
   belongs_to :applicant
   belongs_to :user
 
+  enum email_type: {
+    outbound: "outbound",
+    inbound: "inbound"
+  }
+
   has_rich_text :body
 
   validates :subject, presence: true
 
-  after_create_commit :send_email
+  after_create_commit :send_email, if: :outbound?
 
   def send_email
     ApplicantMailer.contact(email: self).deliver_later
